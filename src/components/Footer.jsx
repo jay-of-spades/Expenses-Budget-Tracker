aa4748c57c90cf41e305ec81b35c2b9eb39d327d
Jay Santarina <jay.santarina@gmail.com>
2024-06-27 15:32:17 +0200

    Deploy to GitHub Pages

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className='p-4 bg-blue-700 text-center'>
-      <p>&copy; 2024 Personal Finance Tracker. All rights reserved.</p>
-    </footer>
-  );
-};
-
-export default Footer;
